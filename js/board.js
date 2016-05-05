02a4be51d0a43fad9f3441a03cd5fe7ef7b5758e
theomarkkuspaul <theo.markkus.paul@gmail.com>
2016-05-04 21:45:15 -0400

    Strike cell functionality working

--- js/board.js
+++ js/board.js
@@ -3,6 +3,19 @@ var Board = function(){
   this.board = {1:[0,0,0],
          2:[0,0,0],
          3:[0,0,0]}
+  // this.lastRow =
 }
 
+Board.prototype.findCellIndexNum = function(rowNum, cellNum){
+  return cellNum - ((parseInt(rowNum) - 1) * 3) - 1;
+};
 
+Board.prototype.cellCoordinates = function(rowNum, cellIndexNum) {
+  var row;
+  row = parseInt(rowNum);
+  return [row, cellIndexNum];
+};
+
+Board.prototype.strikeCell = function(cellCoors, type) {
+  this.board[cellCoors[0]][cellCoors[1]] = type
+};
--- js/tic-tac-toe.js
+++ js/tic-tac-toe.js
@@ -5,10 +5,13 @@ $(document).ready(function(event){
   $('.cell-square').on('click', function(event){
     // event.preventDefault();
     var $target = $(event.target);
-    var cellNum, rowNum;
+    var cellNum, rowNum, coors;
 
     cellNum = $target.attr('id')[4];
     rowNum = $target.parent().parent().parent().attr('id')[3];
+    coors = board.cellCoordinates(rowNum, board.findCellIndexNum(rowNum, cellNum));
+    board.strikeCell(coors, 'X');
+
   });
 
 });
